5c8bd9309ffb3b912cb5b57de0f14006dc1d80c0
Kobe Prado Tuazon <kobeb7952@gmail.com>
2025-11-12 08:19:44 +0800

    Fixed for Deploying

--- utils/auth-guard.js
+++ utils/auth-guard.js
@@ -3,15 +3,15 @@ import supabase from './supabase.js';
 export async function redirectIfLoggedIn() {
   const { data } = await supabase.auth.getSession();
   if (data.session) {
-    window.location.replace('/pages/dashboard/overview/index.html');
-    // window.location.replace('/Residential-App/pages/dashboard/overview/index.html');
+    // window.location.replace('/pages/dashboard/overview/index.html');
+    window.location.replace('/Residential-App/pages/dashboard/overview/index.html');
   }
 }
 
 export async function protectPage() {
   const { data } = await supabase.auth.getSession();
   if (!data.session) {
-    window.location.replace('/pages/login/index.html');
-    // window.location.replace('/Residential-App/pages/login/index.html');
+    // window.location.replace('/pages/login/index.html');
+    window.location.replace('/Residential-App/pages/login/index.html');
   }
 }
